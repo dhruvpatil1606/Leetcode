d421f0fbb6025e07f42c7edd8af641692bcf9e4c
Dhruv patil <149643669+dhruvpatil1606@users.noreply.github.com>
2025-05-30 14:38:53 +0530

    Time: 7 ms (53.13%), Space: 14.9 MB (86.46%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,71 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        if(head==NULL) //if original list is empty than return copy list as empty
+        {
+            return {};
+        }
+
+        Node *newNode=new Node(0); // creating a copy linkd list starting
+
+        Node *newNode_add=new Node(0);
+        newNode_add->next=newNode; // pointing to first node of copylinked list
+        
+        Node *head2=newNode; //returning pointer og copy list                
+
+        Node *head_add=new Node(0); 
+        head_add->next=head; // pointing to first node of original linked list        
+        
+        Node *ptr=head; //assigning pointer to copy original list and next pointer
+        while(ptr!=NULL)
+        {                                    
+            newNode->val=ptr->val;
+            newNode->random=NULL;            // random pointer reamin NULL as for now
+            ptr=ptr->next;
+            
+            if(ptr!=NULL) // if condition for not creating last extra node
+            {
+                newNode->next=new Node(0); //creates a node                
+                newNode=newNode->next;            
+            }
+        }
+
+        // random pointer assigning from here                    
+        
+        ptr=head;    
+        Node *ptr2=head2;    
+        while(ptr!=NULL)
+        {                                       
+                Node *address=ptr->random;
+                Node *ptr3=head;
+                Node *ptr4=head2;
+                while(ptr3!=address)
+                {                    
+                    ptr3=ptr3->next;
+                    ptr4=ptr4->next;
+                }                
+                ptr2->random=ptr4;
+            
+            ptr=ptr->next;
+            ptr2=ptr2->next;
+        }
+        return head2;
+    }
+};
