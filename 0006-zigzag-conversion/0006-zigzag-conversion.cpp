8299a17005daa7fb055967bf6707fa2665c04046
Dhruv patil <149643669+dhruvpatil1606@users.noreply.github.com>
2025-06-05 22:42:11 +0530

    Time: 102 ms (5.03%), Space: 73.3 MB (5.07%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -43,70 +43,20 @@ public:
             j=daigonal_sum;
             count=j;
             daigonal_sum=daigonal_sum+(numRows-1);
-            cout<<"\n\n"<<daigonal_sum<<"\t"<<count<<"\t"<<j<<"\t"<<index<<"\n\n";
+            // cout<<"\n\n"<<daigonal_sum<<"\t"<<count<<"\t"<<j<<"\t"<<index<<"\n\n";
             if(index>len) 
                 break;
        }
 
-        //printing for acknowledgement
-        // for(int i=0; i<numRows; i++){
-        //     for(int j=0; j<str[0].size(); j++)
-        //     {
-        //         cout<<str[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-        
         //making returning string s2
         for(int i=0; i<numRows; i++){
             for(int j=0; j<str[0].size(); j++)
             {
                 if(str[i][j]!=0) s2=s2+str[i][j];
             }
-            cout<<"\n";
+            
         }
 
        return s2;
     }
 };
-
-// class Solution {
-// public:
-//     string convert(string s, int numRows) {
-//         if (numRows == 1) return s; 
-
-//         vector<vector<char>> matrix(numRows, vector<char>(s.size(), ' ')); 
-//         int row = 0, col = 0, direction = 1; 
-
-//         for (char c : s) {
-//             matrix[row][col] = c; 
-//             row += direction; 
-
-//             if (row == 0 || row == numRows - 1) {
-//                 direction *= -1; 
-//             }
-
-//             col++; 
-//         }
-
-//         string result = "";
-//         for (int i = 0; i < numRows; i++) {
-//             for (int j = 0; j < s.size(); j++) {
-//                 if (matrix[i][j] != ' ') {
-//                     result += matrix[i][j];
-//                 }
-//             }
-//         }
-
-//         for(int i=0; i<matrix.size(); i++)
-//         {
-//             for(int j=0; j<matrix[i].size(); j++)
-//             {
-//                 cout<<matrix[i][j]<<" ";
-//             }   
-//         cout<<endl;
-//         }
-
-//         return result;
-//     }
-// };
